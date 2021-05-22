96d99b82625ec57474329fa6dc6b7b69f1d0303d
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-05-22 17:02:10 -0300

    Adicionando novas rotas no Drawer do Home

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,13 +2,34 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import Home from '../pages/Home'
+import New from '../pages/New'
+import Profile from '../pages/Profile'
 
 const AppDrawer = createDrawerNavigator()
 
 function AppRoutes() {
     return(
-        <AppDrawer.Navigator>
+        <AppDrawer.Navigator
+        drawerStyle={{
+            backgroundColor: '#171717'
+        }}
+        drawerContentOptions={{
+            labelStyle:{
+                fontWeight: 'bold'
+            },
+            activeTintColor: '#fff',
+            activeBackgroundColor: '#d2a83e',
+            inactiveBackgroundColor: '#000',
+            inactiveTintColor: '#ddd',
+            itemStyle: {
+                marginVertical: 5
+            }
+
+        }}
+        >
             <AppDrawer.Screen name="Home" component={Home}/>
+            <AppDrawer.Screen name="Registrar" component={New}/>
+            <AppDrawer.Screen name="Perfil" component={Profile}/>
         </AppDrawer.Navigator>
     )
 }
